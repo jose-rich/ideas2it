63ff3b6207dc843ec34d264d33ba4bc2aa0ff671
jose-rich <joserichard@ideas2it.com>
2023-05-04 13:17:27 +0530

    name changed

--- authentication-service/src/main/java/com/i2it/security/AuthenticationApplication.java
+++ authentication-service/src/main/java/com/i2it/security/AuthenticationApplication.java
@@ -9,10 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
 	@Bean
